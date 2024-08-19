f551bec8c98eb0c0f0c5c847dc81aa2a500fc6ba
Andrzej Fricze <a.fricze@pm.me>
2024-08-19 16:32:09 +0200

    Create new Remotion video

---
+++ src/Main.tsx
@@ -0,0 +1,108 @@
+import {
+	AbsoluteFill,
+	interpolate,
+	Series,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+import {ProgressBar} from './ProgressBar';
+import {CodeTransition} from './CodeTransition';
+import {HighlightedCode} from 'codehike/code';
+import {ThemeColors, ThemeProvider} from './calculate-metadata/theme';
+import {useMemo, useRef} from 'react';
+import {RefreshOnCodeChange} from './ReloadOnCodeChange';
+import {verticalPadding} from './font';
+import './scroller.css';
+
+export type Props = {
+	steps: HighlightedCode[] | null;
+	themeColors: ThemeColors | null;
+	codeWidth: number | null;
+};
+
+export const Main: React.FC<Props> = ({steps, themeColors, codeWidth}) => {
+	if (!steps) {
+		throw new Error('Steps are not defined');
+	}
+
+	const {durationInFrames} = useVideoConfig();
+	const stepDuration = durationInFrames / steps.length;
+	const transitionDuration = 30;
+
+	if (!themeColors) {
+		throw new Error('Theme colors are not defined');
+	}
+
+	const outerStyle: React.CSSProperties = useMemo(() => {
+		return {
+			backgroundColor: themeColors.background,
+		};
+	}, [themeColors]);
+
+	const style: React.CSSProperties = useMemo(() => {
+		return {
+			padding: `${verticalPadding}px 0px`,
+		};
+	}, []);
+
+	const frame = useCurrentFrame();
+	const scroll = interpolate(frame, [0, 20], [0, 1]);
+
+	const scrollRef = useRef<HTMLDivElement>(null);
+	scrollRef.current?.scroll(0, scroll * 100);
+
+	return (
+		<ThemeProvider themeColors={themeColors}>
+			<AbsoluteFill style={outerStyle}>
+				<div
+					style={{
+						width: codeWidth || '100%',
+						margin: 'auto',
+						position: 'absolute',
+						left: '40px',
+					}}
+				>
+					<ProgressBar steps={steps} />
+					<AbsoluteFill style={style}>
+						<Series>
+							{steps.map((step, index) => (
+								<Series.Sequence
+									key={index}
+									layout="none"
+									durationInFrames={stepDuration}
+									name={step.meta}
+								>
+									<CodeTransition
+										oldCode={steps[index - 1]}
+										newCode={step}
+										durationInFrames={transitionDuration}
+									/>
+								</Series.Sequence>
+							))}
+						</Series>
+					</AbsoluteFill>
+				</div>
+
+				<div ref={scrollRef} style={{}} className="scroller-parent">
+					<h1 className="scroller">
+						Code transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition Code
+						transition Code transition Code transition Code transition
+					</h1>
+				</div>
+			</AbsoluteFill>
+			<RefreshOnCodeChange />
+		</ThemeProvider>
+	);
+};
---
+++ src/Root.tsx
@@ -0,0 +1,28 @@
+import {Composition} from 'remotion';
+import {Main} from './Main';
+
+import {calculateMetadata} from './calculate-metadata/calculate-metadata';
+import {schema} from './calculate-metadata/schema';
+
+export const RemotionRoot = () => {
+	return (
+		<Composition
+			id="Main"
+			component={Main}
+			defaultProps={{
+				steps: null,
+				themeColors: null,
+				theme: 'github-dark' as const,
+				codeWidth: null,
+				width: {
+					type: 'auto',
+				},
+			}}
+			fps={30}
+			width={1920}
+			height={1080}
+			calculateMetadata={calculateMetadata}
+			schema={schema}
+		/>
+	);
+};
